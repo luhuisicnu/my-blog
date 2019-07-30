74159078a57ed989ec866ec6970032f4c957f765
luhuisicnu <luhuisicnu@163.com>
2019-07-30 10:17:27 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-      <v-card v-for="(article, index) in queryArticles" :key="article.title" class="my-5">
-        <!-- <v-img
-          :src="require(`@/assets/images/${article.image}`)"
-          aspect-ratio="3.75"
-        ></v-img> -->
-        <v-parallax :src="require(`@/assets/images/${article.image}`)" aspect-ratio="3.75"></v-parallax>
-        <v-card-title>
-          <h1><router-link :to="{ name: 'article', params: { index }}">{{ article.title }}</router-link></h1>
-        </v-card-title>
-        <v-card-text>{{ article.summary }}</v-card-text>
-      </v-card>
+      <template v-for="(article, index) in queryArticles">
+        <v-card v-if="!article.disabled" :key="article.title" class="my-5">
+          <!-- <v-img
+            :src="require(`@/assets/images/${article.image}`)"
+            aspect-ratio="3.75"
+          ></v-img> -->
+          <v-parallax :src="require(`@/assets/images/${article.image}`)" aspect-ratio="3.75"></v-parallax>
+          <v-card-title>
+            <h1><router-link :to="{ name: 'article', params: { index }}">{{ article.title }}</router-link></h1>
+          </v-card-title>
+          <v-card-text>{{ article.summary }}</v-card-text>
+        </v-card>
+      </template>
     </div>
 </template>
 
@@ -24,11 +26,26 @@ export default {
       ...mapState(['articles']),
       queryArticles () {
         if (this.$route.params.tag) {
-          return this.articles.filter((item) => {
-            return item.tag.indexOf(this.$route.params.tag) > -1
+          return this.articles.map((item) => {
+            const newItem = this.copy(item)
+            if (item.tag.indexOf(this.$route.params.tag) === -1) {
+              newItem.disabled = true
+            } else {
+              newItem.disabled = false
+            }
+            return newItem
           })
         }
         return this.articles
+      }
+    },
+    methods: {
+      copy (obj) {
+        const newObj = {}
+        for (let item in obj) {
+          newObj[item] = obj[item]
+        }
+        return newObj
       }
     },
 }
